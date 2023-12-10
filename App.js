82966ba63f49d5482b5ec145b8d90aac9926207c
Nguyễn Tấn Thy <kuthy2728@gmail.com>
2023-12-11 02:52:44 +0700

    Comit

---
+++ App.js
@@ -0,0 +1,84 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, useColorScheme } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+import { useCallback, useMemo } from "react";
+import {
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Cart from "./screens/Home/Cart";
+import Intro from "./screens/Intro";
+import Guide from "./screens/Begin/Guide";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ForgotPassword from "./screens/ForgotPassword";
+import ResetPassword from "./screens/ResetPassword";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import ProductDetail from "./components/products/ProductDetail";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import TabsNavigator from "./navigation/TabsNavigator";
+import PhoneScreen from "./screens/PhoneScreen";
+import EmailScreen from "./screens/EmailScreen";
+import NewPassword from "./screens/NewPassword";
+import AppNavigator from "./navigation/AppNavigator";
+import { AppContextProvider } from "./components/ultil/AppContext";
+import FilterScreen from "./screens/Home/FilterScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  const colorScheme = useColorScheme();
+
+  const theme = useMemo(
+    () =>
+      colorScheme === "dark"
+        ? {
+            ...DarkTheme,
+            colors: {
+              ...DarkTheme.colors,
+              primary: "#fff",
+              text: "#fff",
+            },
+          }
+        : {
+            ...DefaultTheme,
+            colors: {
+              ...DefaultTheme.colors,
+              background: "#fff",
+              text: "#191919",
+              border: "#D9D9D9",
+              primary: "#191919",
+            },
+          },
+    [colorScheme]
+  );
+  return (
+    <SafeAreaProvider style={{ flex: 1 }}>
+      <GestureHandlerRootView style={styles.container}>
+        <AppContextProvider>
+          <NavigationContainer theme={theme}>
+            <BottomSheetModalProvider>
+              <AppNavigator />
+            </BottomSheetModalProvider>
+          </NavigationContainer>
+        </AppContextProvider>
+      </GestureHandlerRootView>
+      {/* <FilterScreen></FilterScreen> */}
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  textstyle: {
+    fontFamily: "regular",
+    fontSize: 20,
+  },
+});
